216ed4e3882156374585c314865cec7c76b94e38
Alyssa Margalit <alyssam0@usc.edu>
2022-04-12 11:49:19 -0700

    get 3 distances

--- rsDistance/clickTest.py
+++ rsDistance/clickTest.py
@@ -12,32 +12,50 @@ ret, depth_frame, color_frame = dc.get_frame()
 # show image
 cv2.imshow('image', color_frame)
    
+
+    
+
 #define the events for the
 # mouse_click.
+pointer = (0,0)
 point1 = (0,0)
 point2 = (0,0)
 point3 = (0,0)
+d1 = 0
+d2 = 0
+d3 = 0
 state = 1
 
 def mouse_click(event, x, y, 
                 flags, param): 
     # to check if left mouse 
+    global pointer
     global point1
     global point2
     global point3
     global state
+    global d1
+    global d2
+    global d3
+    global distance
 
+    distance = depth_frame[pointer[1],pointer[0]]
+
+    pointer = (x,y)
     if event == cv2.EVENT_LBUTTONDOWN:
         font = cv2.FONT_HERSHEY_TRIPLEX
         LB = 'Left Button'
         if state ==1 :
             point1 = (x,y)
+            d1 = distance
             state = 2
         elif state ==2:
             point2 = (x,y)
+            d2 = distance
             state = 3
         elif state ==3:
             point3 = (x,y)
+            d3 = distance
             state = 1
 
     
@@ -47,19 +65,27 @@ cv2.setMouseCallback('image', mouse_click)
 while True:
     ret, depth_frame, color_frame = dc.get_frame()
     
+    cv2.circle(color_frame, pointer, 4,(0,0,255))
+
     cv2.circle(color_frame, point1, 4,(255,0,0))
-    cv2.circle(color_frame, point2, 4,(0,255,0))
-    cv2.circle(color_frame, point3, 4,(0,255,255))
-
-
-    #show distance from a specific point
+    cv2.putText(color_frame,"{}mm".format(d1),(point1[0],point1[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
     
-    #cv2.circle(color_frame, point, 4,(0,0,255))
-    #if depth_frame[point[1],point[0]] != 0:
-       # distance = depth_frame[point[1],point[0]]
-    #cv2.putText(color_frame,"{}mm".format(distance),(point[0],point[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
+    cv2.circle(color_frame, point2, 4,(0,255,0))
+    cv2.putText(color_frame,"{}mm".format(d2),(point2[0],point2[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
+    
+    cv2.circle(color_frame, point3, 4,(0,255,255))
+    cv2.putText(color_frame,"{}mm".format(d3),(point3[0],point3[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
+    
 
-    #print(distance)
+    if depth_frame[pointer[1],pointer[0]] != 0:
+        distance = depth_frame[pointer[1],pointer[0]]
+        cv2.putText(color_frame,"{}mm".format(distance),(pointer[0],pointer[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
+    else:
+      cv2.putText(color_frame,"?",(pointer[0],pointer[1]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2)
+      
+
+
+
     cv2.imshow("image", color_frame)
 
     key = cv2.waitKey(1)
